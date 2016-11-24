fb2f9c849a425f68f8973b9fcea794687452a86d
yangshaoshuai <18353626761@163.com>
2016-11-24 17:34:13 +0800

    test

---
+++ app/src/main/java/com/phone1000/administrator/advancedday14git/MainActivity.java
@@ -0,0 +1,18 @@
+package com.phone1000.administrator.advancedday14git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
